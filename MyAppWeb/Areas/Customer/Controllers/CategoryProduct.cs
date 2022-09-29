d7347baf3a170d8aa7941dd4316c4fcd6eb32526
ashiksarker125 <2018000000125@seu.edu.bd>
2022-09-29 22:01:15 +0600

    berfore add to cart

---
+++ MyAppWeb/Areas/Customer/Controllers/CategoryProduct.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyApp.Models;
+using MyyApp.DataAccessLayer.Data;
+
+namespace MyAppWeb.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class CategoryProduct : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryProduct(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index(int id)
+        {
+            IEnumerable <ProductDb> products = _context.ProductDbs.Where(x => x.CategoryId == id).ToList();
+            return View(products);
+        }
+    }
+}
---
+++ MyAppWeb/Component/CategoryViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyApp.Models;
+using MyyApp.DataAccessLayer.Data;
+
+namespace MyAppWeb.Component
+{
+    public class CategoryViewComponent :ViewComponent
+    {
+        private readonly ApplicationDbContext _context;
+        public CategoryViewComponent(ApplicationDbContext applicationDbContext)
+        {
+            _context = applicationDbContext;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            IEnumerable<Category> categorylist = _context.Categories; 
+            return View("Index", _context.Categories.ToList());
+        }
+    }
+}
